e92220980a3c5dc7b03e407485fe0d5e7c5c706a
Barbara Frosik <bfrosik@anl.gov>
2020-03-05 17:00:12 -0600

    Update bridge_cpu.pyx

--- reccdi/src_py/cyth/bridge_cpu.pyx
+++ reccdi/src_py/cyth/bridge_cpu.pyx
@@ -6,10 +6,10 @@
 
 
 # distutils: language = c++
-# distutils: include_dirs = ['reccdi/include', '/home/beams/CXDUSER/CDI/arrayfire/include', '/home/beams/CXDUSER/CDI/libconfig/include',]
+# distutils: include_dirs = ['reccdi/include', 'AF_DIR/include', 'LC_DIR/include',]
 # distutils: sources = ['reccdi/src_cpp/bridge.cpp', 'reccdi/src_cpp/manager.cpp', 'reccdi/src_cpp/parameters.cpp', 'reccdi/src_cpp/pcdi.cpp', 'reccdi/src_cpp/resolution.cpp', 'reccdi/src_cpp/state.cpp', 'reccdi/src_cpp/support.cpp', 'reccdi/src_cpp/util.cpp', 'reccdi/src_cpp/worker.cpp']
 # distutils: libraries = ['afcpu',  'config++',]
-# distutils: library_dirs = ['/home/beams/CXDUSER/CDI/arrayfire/lib64', '/home/beams/CXDUSER/CDI/libconfig/lib',]
+# distutils: library_dirs = ['AF_DIR/lib64', 'LC_DIR/lib',]
 
 
 from libcpp.vector cimport vector
