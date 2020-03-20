2a02dae104d5cc26f8edd4f1d226f3496265e11a
bfrosik <bfrosik@anl.gov>
2020-03-20 11:33:36 -0500

    added initialization for Mac

---
+++ init_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo -n "enter ArrayFire installation directory > "
+read af_dir
+AF='AF_DIR'
+AFLIB='AF_LIB'
+sed -i 's?'$AF'?'$af_dir'?g' reccdi/src_py/cyth/*.pyx
+sed -i 's?'$AFLIB'?'$af_dir/lib'?g' reccdi/src_py/cyth/*.pyx
+
+echo -n "enter LibConfig installation directory > "
+read lc_dir
+LC='LC_DIR'
+sed -i 's?'$LC'?'$lc_dir'?g' reccdi/src_py/cyth/*.pyx
+
+export LD_LIBRARY_PATH=$lc_dir/lib:/usr/local/lib:$af_dir/lib
+
+lib_path='LIB_PATH'
+sed -i 's?'$lib_path'?'$lc_dir/lib:$af_dir/lib'?g' bin/everything.sh
+sed -i 's?'$lib_path'?'$lc_dir/lib:$af_dir/lib'?g' bin/run_rec.sh
+sed -i 's?'$lib_path'?'$lc_dir/lib:$af_dir/lib'?g' bin/cdi_window.sh
+
+python setup.py build_ext --inplace
+python setup.py install
