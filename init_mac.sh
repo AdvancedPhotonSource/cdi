06fde6c16a09c2f367cd507588a9bb958abbf3c7
bfrosik <bfrosik@anl.gov>
2020-03-20 18:10:37 -0500

    fixed init_mac.sh

--- init_mac.sh
+++ init_mac.sh
@@ -2,22 +2,16 @@
 
 echo -n "enter ArrayFire installation directory > "
 read af_dir
-AF='AF_DIR'
-AFLIB='AF_LIB'
-sed -i 's?'$AF'?'$af_dir'?g' reccdi/src_py/cyth/*.pyx
-sed -i 's?'$AFLIB'?'$af_dir/lib'?g' reccdi/src_py/cyth/*.pyx
+sed -i '' "s#AF_DIR#$af_dir#g" reccdi/src_py/cyth/*.pyx
+sed -i '' "s#AF_LIB#$af_dir/lib#g" reccdi/src_py/cyth/*.pyx
 
 echo -n "enter LibConfig installation directory > "
 read lc_dir
-LC='LC_DIR'
-sed -i 's?'$LC'?'$lc_dir'?g' reccdi/src_py/cyth/*.pyx
+sed -i '' "s#LC_DIR#$lc_dir#g" reccdi/src_py/cyth/*.pyx
 
-export DYLD_LIBRARY_PATH=$lc_dir/lib:/usr/local/lib:$af_dir/lib
+export DYLD_LIBRARY_PATH=$lc_dir/lib:$af_dir/lib
 
-lib_path='LIB_PATH'
-sed -i 's?'$lib_path'?'$lc_dir/lib:$af_dir/lib'?g' bin/everything.sh
-sed -i 's?'$lib_path'?'$lc_dir/lib:$af_dir/lib'?g' bin/run_rec.sh
-sed -i 's?'$lib_path'?'$lc_dir/lib:$af_dir/lib'?g' bin/cdi_window.sh
+sed -i '' "s#LIB_PATH#$lc_dir/lib:$af_dir/lib#g" bin/cdi_window.sh
 
 python setup.py build_ext --inplace
 python setup.py install
