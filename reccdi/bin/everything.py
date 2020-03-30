b16fa8d3dd0cce2a858c66694bac8f21424b847e
bfrosik <bfrosik@anl.gov>
2020-03-30 17:34:48 -0500

    cleaned up scripts

--- reccdi/bin/everything.py
+++ reccdi/bin/everything.py
@@ -1,4 +1,3 @@
-import reccdi.bin.setup_34idc as set
 import reccdi.bin.run_prep_34idc as prep
 import reccdi.bin.format_data as dt
 import reccdi.bin.run_rec as rec
@@ -6,36 +5,31 @@ import reccdi.bin.run_disp as dsp
 import sys
 import argparse
 
-def run_all(dev, experiment_dir):
+def run_all(dev, experiment_dir, **kwargs):
     prep.set_prep(experiment_dir)
     dt.data(experiment_dir)
-    rec.manage_reconstruction(dev, experiment_dir)
-    dsp.to_vtk(experiment_dir)
+    if 'rec_id' in kwargs:
+        rec.manage_reconstruction(dev, experiment_dir, kwargs['rec_id'])
+    else:
+        rec.manage_reconstruction(dev, experiment_dir)
+    if 'results_dir' in kwargs:
+        dsp.to_vtk(experiment_dir, kwargs['results_dir'])
+    else:
+        dsp.to_vtk(experiment_dir)
 
 def main(arg):
     parser = argparse.ArgumentParser()
     parser.add_argument("dev", help="processor to run on (cpu, opencl, cuda)")
-    parser.add_argument("prefix", help="prefix id")
-    parser.add_argument("scans", help="scans to preocess")
-    parser.add_argument("conf_dir", help="directory with configuration files")
-    parser.add_argument('--specfile', action='store')
-    parser.add_argument('--copy_prep', action='store_true')
+    parser.add_argument("experiment_dir", help="experiment directory")
+    parser.add_argument("--rec_id", help="reconstruction id, a prefix to '_results' directory")
+    parser.add_argument("--results_dir", help="directory in experiment that has a tree (or leaf) with reconstruction results which will be visualized")
+
     args = parser.parse_args()
     dev = args.dev
-    prefix = args.prefix
-    scans = args.scans
-    conf_dir = args.conf_dir
-    if args.specfile and os.path.isfile(args.specfile):
-        specfile = args.specfile
-    else:
-        specfile = None
-
-    experiment_dir = set.setup_rundirs(prefix, scans, conf_dir, copy_prep=args.copy_prep, specfile=specfile)
-    print ('experiment dir', experiment_dir)
-    run_all(dev, experiment_dir)
+    experiment_dir = args.experiment_dir
+    run_all(dev, experiment_dir, rec_id=args.rec_id, results_dir=args.results_dir)
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
 
-#python everything.py device prefix scans conf_dir
