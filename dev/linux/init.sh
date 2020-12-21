5073272c4b625a6d7c87f60b31f72aded41272cd
bfrosik <bfrosik@anl.gov>
2020-12-21 10:42:10 -0600

    modified init.sh and templates

--- dev/linux/init.sh
+++ dev/linux/init.sh
@@ -11,17 +11,8 @@ AFLIB='AF_LIB'
 sed -i 's?'$AF'?'$af_dir'?g' reccdi/src_py/cyth/*.pyx
 sed -i 's?'$AFLIB'?'$af_dir/lib64'?g' reccdi/src_py/cyth/*.pyx
 
-echo -n "enter LibConfig installation directory > "
-read lc_dir
-LC='LC_DIR'
-sed -i 's?'$LC'?'$lc_dir'?g' reccdi/src_py/cyth/*.pyx
 
-export LD_LIBRARY_PATH=$lc_dir/lib:$af_dir/lib64
-
-lib_path='LIB_PATH'
-
-sed -i 's?'$lib_path'?'$LD_LIBRARY_PATH'?g' bin/setenv.sh
-sed -i 's?'$lib_path'?'$LD_LIBRARY_PATH'?g' bin/cdi_window.sh
+export LD_LIBRARY_PATH=$af_dir/lib64
 
 python setup.py build_ext --inplace
 python setup.py install
