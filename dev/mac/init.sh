e4d582a2172cdd01519a594a7f44d6a3d2043742
Barbara Frosik <bfrosik@anl.gov>
2020-12-22 10:57:43 -0600

    Update init.sh

--- dev/mac/init.sh
+++ dev/mac/init.sh
@@ -5,13 +5,7 @@ read af_dir
 sed -i '' "s#AF_DIR#$af_dir#g" reccdi/src_py/cyth/*.pyx
 sed -i '' "s#AF_LIB#$af_dir/lib#g" reccdi/src_py/cyth/*.pyx
 
-echo -n "enter LibConfig installation directory > "
-read lc_dir
-sed -i '' "s#LC_DIR#$lc_dir#g" reccdi/src_py/cyth/*.pyx
-
-export DYLD_LIBRARY_PATH=$lc_dir/lib:$af_dir/lib
-
-sed -i '' "s#LIB_PATH#$lc_dir/lib:$af_dir/lib#g" bin/cdi_window.sh
+export DYLD_LIBRARY_PATH=$af_dir/lib
 
 python setup.py build_ext --inplace
 python setup.py install
